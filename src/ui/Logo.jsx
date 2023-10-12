8e5ccbf1dd8020db1453b75f47fabbcc90abf2ae
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-10-12 13:25:47 +0530

    Handling Form Errors

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -3,7 +3,13 @@ import styled from "styled-components";
 const StyledLogo = styled.div`
   text-align: center;
 `;
-
+const LogoTest = styled.p`
+  font-size: 2rem;
+  font-weight: 600;
+  line-height: 0.1rem;
+  letter-spacing: 0.07rem;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+`;
 const Img = styled.img`
   height: 9.6rem;
   width: auto;
@@ -13,6 +19,7 @@ function Logo() {
   return (
     <StyledLogo>
       <Img src="/ForestHotel.png" height="50px" width="50px" alt="Logo" />
+      <LogoTest>Forest Club House</LogoTest>
     </StyledLogo>
   );
 }
